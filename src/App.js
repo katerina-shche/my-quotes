5bab0dc342873f573a3a035b61a1209cede2f78e
katerina-shche <asti.katerina@gmail.com>
2023-04-12 18:33:16 +0200

    final touch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+//styles
+import { useEffect, useState } from 'react';
 import './App.css';
+//icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const colors = ['pink', '#D35E0A', '#934107', '#0a7fd3', '#f1ceb5', '#d3c30a']
+  const [quote, setQuote] = useState(null)
+  const [author, setAuthor] = useState('waiting for author')
+  const [color, setColor] = useState(colors[0])
+
+  const handleClick = () => {
+    //change the color
+    if (colors.indexOf(color) === colors.length - 1) {
+      setColor(colors[0])
+    } else {setColor(colors[colors.indexOf(color) + 1])}
+    //fetch the quote
+    try {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(json => {
+        setQuote(json.content)
+        setAuthor(json.author)
+      })
+    } catch(err) {
+      setQuote('err.message')
+    }
+  }
+
+  useEffect(() => {
+    handleClick()
+  }, [])
+
+
+  if (quote) {return (
+    
+    <div id='colorful-background' style={{backgroundColor: color}}>
+    <div id="quote-box">
+      <div id="quote-place">
+      <FontAwesomeIcon icon={faQuoteLeft} style={{color: color, fontSize: 30, marginRight: 10}} />
+        <span id="text" style={{color: color}}>  {quote}</span>
+      </div>
+      <div id="author" style={{color: color}}>{author}</div>
+      <div id="buttons">
+        <button style={{backgroundColor: color}}><a id="tweet-quote" href='https://twitter.com/intent/tweet'><FontAwesomeIcon icon={faTwitter} /></a></button>
+      
+        <button id="new-quote" 
+        onClick={handleClick}
+        style={{backgroundColor: color}}>
+          New quote
+        </button>
+      </div>
     </div>
-  );
+    <h4>by katerina.shche</h4>
+    </div>
+    
+  )
+  }
 }
 
 export default App;
+
